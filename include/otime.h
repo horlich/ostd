571ac8465052a50ce5410e09f15a6111d1b96255
Herbert Orlich <herbert@orlich.at>
2022-01-20 10:36:39 +0100

    OTime verbessert

--- include/otime.h
+++ include/otime.h
@@ -32,17 +32,36 @@ bool istSchaltjahr(int jahr);
 
 
 class Year {
-	//
-	int jz;
+   //
+   size_t jz;
 
 public:
-	Year(size_t zahl = 0);
+   Year(size_t zahl = 0);
 
-	inline int jj() const { return jz; }
+   inline int jj() const
+   {
+      return jz;
+   }
 
-	inline bool istSchaltjahr() const { return Zeit::istSchaltjahr(jz); }
+   inline bool istSchaltjahr() const
+   {
+      return Zeit::istSchaltjahr(jz);
+   }
 
-	Year& operator()(size_t zahl);
+   Year& operator()(size_t zahl);
+
+   Year& operator=(const Year& j);
+
+   /* Gültige Jahreszahl? */
+   operator bool() const;
+
+   constexpr Year& operator++();   /* Präfix-Inkrementor */
+
+   Year operator++(int);   /* Postfix-Inkrementor */
+
+   constexpr Year& operator--();   /* Präfix-Inkrementor */
+
+   Year operator--(int);   /* Postfix-Inkrementor */
 };
 
 
@@ -52,31 +71,50 @@ public:
 
 
 class Month {
-	//
-	size_t mz;
-	Year jahr;
+   //
+   size_t mz;
+   Year jahr;
 
 public:
-	// validiert nicht!
-	Month(size_t zahl = 0, const Year& j = Year(0));
+   // validiert nicht!
+   Month(size_t zahl = 0, const Year& j = Year(0));
 
-	Month(const OTime* ptr);
+   /* Wenn nullptr übergeben, wird aktueller Monat erzeugt: */
+   Month(const OTime&);
 
-	// validiert nicht!
-	Month& operator()(size_t mz, const Year&);
+   // validiert nicht!
+   Month& operator()(size_t mz, const Year&);
 
-	// Validiere: Gültige Monatszahl?
-	operator bool() const;
+   // Validiere: Gültige Monatszahl?
+   operator bool() const;
 
-	inline int mm() const { return mz; }
+   Month& operator++();
 
-	inline const Year& year() const { return jahr; }
+   Month operator++(int);
 
-	// Anzahl Tage dieses Monats:
-	int days() const;
+   Month& operator--();
 
-	// wirft IllegalArgumentException:
-	void validate() const;
+   Month operator--(int);
+
+   inline int mm() const
+   {
+      return mz;
+   }
+
+   inline int jj() const
+   {
+      return jahr.jj();
+   }
+
+   std::string name() const;
+
+   inline const Year& year() const
+   {
+      return jahr;
+   }
+
+   // Anzahl Tage dieses Monats:
+   int days() const;
 };
 
 
@@ -89,52 +127,76 @@ public:
 
 class Day {
 private:
-	Month monat;
-	int tz{0};
+   Month monat;
+   size_t tz{0};
 
-	inline void init(const Zeit::OTime*);
+   inline void init(const Zeit::OTime*);
 
-	inline void parse(std::istream&, const Zeit::OTime*); // validiert nicht!
+   inline void parse(std::istream&, const Zeit::OTime*); // validiert nicht!
 
 protected:
-	virtual void setValues(int d, int m, int j) {
-		tz = d;
-		monat = Month(m, j);
-	}
+   virtual void setValues(int d, int m, int j)
+   {
+      tz = d;
+      monat = Month(m, j);
+   }
 
 public:
-	Day(); /* erzeugt 00.00.0000 */
+   Day(); /* erzeugt 00.00.0000 */
 
-	/* wirft IllegalArgumentException, wenn Datum nicht erkannt wird: */
-	Day(const std::string& parse, const Zeit::OTime* now = nullptr);
+   /* wirft IllegalArgumentException, wenn Datum nicht erkannt wird: */
+   Day(const std::string& parse, const Zeit::OTime* now = nullptr);
 
-	Day(const Zeit::OTime&);
+   Day(const Zeit::OTime&);
 
-	// validiert nicht!
-	Day(size_t t, size_t m, size_t j);
+   // validiert nicht!
+   Day(size_t t, size_t m, size_t j);
 
-	// validiert nicht!
-	Day(size_t tt, const Month& = Month());
+   // validiert nicht!
+   Day(size_t tt, const Month& = Month());
 
-	virtual ~Day() = default;
+   virtual ~Day() = default;
 
-	OTime getTime() const;
+   OTime getTime() const;
 
-	int tt() const { return tz; }
+   inline int tt() const
+   {
+      return tz;
+   }
 
-	std::string toString() const;
+   inline int mm() const
+   {
+      return monat.mm();
+   }
 
-	std::string toSQL() const;
+   inline int jj() const
+   {
+      return monat.jj();
+   }
 
-	Day deltaDays(int i);
+   inline const Month& getMonth() { return monat; }
 
-	/* Enthält gültigen Datumswert? */
-	operator bool() const;
+   std::string toString() const;
 
-	friend std::istream& operator>>(std::istream& is, Day& td);
+   std::string toSQL() const;
+
+   Day deltaDays(int i);
+
+   /* Enthält gültigen Datumswert? */
+   operator bool() const;
+
+   Day& operator++();
+
+   Day operator++(int);
+
+   Day& operator--();
+
+   Day operator--(int);
+
+   friend std::istream& operator>>(std::istream& is, Day& td);
 };
 
-// wirft IllegalArgumentException:
+/* validiert nicht! */
 Day parseDay(std::istream&, const Zeit::OTime* = nullptr);
 
 std::ostream& operator<<(std::ostream& os, const Day& td);
@@ -143,20 +205,22 @@ Day today();
 
 
 
+
+
 class SQLDay : public Day {
 public:
 //	SQLDay() : Day(0,0,0) {} // Erzeugt einen ungültigen Wert!
    SQLDay() = default;
 
-	/* Argument muß strikt im SQL-Format
-	 * sein, sonst verhält sich das Programm
-	 * undefiniert! */
-	SQLDay(std::string sql);
+   /* Argument muß strikt im SQL-Format
+    * sein, sonst verhält sich das Programm
+    * undefiniert! */
+   SQLDay(std::string sql);
 
-	virtual ~SQLDay() = default;
+   virtual ~SQLDay() = default;
 
-	using Day::setValues;
-	virtual void setValues(const std::string& sql);
+   using Day::setValues;
+   virtual void setValues(const std::string& sql);
 };
 
 
@@ -165,67 +229,112 @@ public:
 
 
 class OTime {
-	/*****************************************************************
-	 * Dazu gibt es eine Boost-Bibliothek:
-	 * https://dieboostcppbibliotheken.de/boost.datetime-kalenderdaten
-	 *****************************************************************/
-    time_t myTime;
-    struct tm localTime;
-    inline void init(long int);
+   /*****************************************************************
+    * Dazu gibt es eine Boost-Bibliothek:
+    * https://dieboostcppbibliotheken.de/boost.datetime-kalenderdaten
+    *****************************************************************/
+   time_t myTime;
+   struct tm localTime;
+   inline void init(long int);
 
 public:
-    OTime();
+   OTime();
 
-    OTime(const char* timestr, const char* format);
+   OTime(const char* timestr, const char* format);
 
-    OTime(int tag, int monat, int jahr);
+   OTime(int tag, int monat, int jahr);
 
-    OTime(long int epoch);
+   OTime(long int epoch);
 
-    long int epoch() const { return static_cast<long int>(myTime); }
+   long int epoch() const
+   {
+      return static_cast<long int>(myTime);
+   }
 
-    // Siehe dazu man localtime...
-    struct tm localtime() const;
+   // Siehe dazu man localtime...
+   struct tm localtime() const;
 
-    inline int tt() const { return localTime.tm_mday; }
+   inline int tt() const
+   {
+      return localTime.tm_mday;
+   }
 
-    inline int mm() const { return localTime.tm_mon + 1; }
+   inline int mm() const
+   {
+      return localTime.tm_mon + 1;
+   }
 
-    inline int jj() const { return localTime.tm_year + 1900; }
+   inline int jj() const
+   {
+      return localTime.tm_year + 1900;
+   }
 
-    inline Year getYear() const { return Year(jj()); }
+   inline Year getYear() const
+   {
+      return Year(jj());
+   }
 
-    inline Month getMonth() const { return Month(mm(), getYear()); }
+   inline Month getMonth() const
+   {
+      return Month(mm(), getYear());
+   }
 
-    inline Day getDay() const { return Day(tt(), getMonth()); }
+   inline Day getDay() const
+   {
+      return Day(tt(), getMonth());
+   }
 
-    Day deltaDays(int d) const;
+   Day deltaDays(int d) const;
 
-    // Siehe dazu man strftime...
-    size_t formatiere(char* chbuf, size_t bufsize, const char* format) const;
+   // Siehe dazu man strftime...
+   size_t formatiere(char* chbuf, size_t bufsize, const char* format) const;
 
-    std::string formatiere(const char* format, size_t capacity) const;
+   std::string formatiere(const char* format, size_t capacity) const;
 
-    inline std::string date10() const { return formatiere("%d.%m.%Y", 10); }
+   inline std::string date10() const
+   {
+      return formatiere("%d.%m.%Y", 10);
+   }
 
-    inline std::string now19() const { return formatiere("%d.%m.%Y %H:%M:%S", 19); }
+   inline std::string now19() const
+   {
+      return formatiere("%d.%m.%Y %H:%M:%S", 19);
+   }
 
-    inline std::string now8() const { return formatiere("%H:%M:%S", 8); }
+   inline std::string now8() const
+   {
+      return formatiere("%H:%M:%S", 8);
+   }
 
-    inline std::string timesql16() const { return formatiere("%Y-%m-%d %H:%M", 16); }
+   inline std::string timesql16() const
+   {
+      return formatiere("%Y-%m-%d %H:%M", 16);
+   }
 
-    inline std::string timesql19() const { return formatiere("%Y-%m-%d %H:%M:%S", 19); }
+   inline std::string timesql19() const
+   {
+      return formatiere("%Y-%m-%d %H:%M:%S", 19);
+   }
 
-    // Timestamp mit Format 8.4
-    inline std::string tmstp84() const { return formatiere("%Y%m%d.%H%M", 13); }
+   // Timestamp mit Format 8.4
+   inline std::string tmstp84() const
+   {
+      return formatiere("%Y%m%d.%H%M", 13);
+   }
 
-    inline std::string getDatum() const { return date10(); }
+   inline std::string getDatum() const
+   {
+      return date10();
+   }
 };
 
 
 std::ostream& operator<<(std::ostream& os, OTime& t);
 
 OTime now();
+
+/* Testet alle Funktionen durch: */
+void testAll();
 
 
 } // namespace Zeit
--- src/otime.cpp
+++ src/otime.cpp
@@ -15,19 +15,27 @@ namespace Zeit {
  *                   Statische Methoden:
  * */
 
-bool setAtLocale() { return (setlocale(LC_TIME, "de_AT.UTF-8") != 0); }
-
-
-bool istSchaltjahr(int jahr) {
-	return  (( (jahr % 4 == 0) && (jahr % 100 != 0) ) || (jahr % 400 == 0));
+bool setAtLocale()
+{
+   return (setlocale(LC_TIME, "de_AT.UTF-8") != 0);
 }
 
-Day today() {
-	OTime ot;
-	return ot.getDay();
+
+bool istSchaltjahr(int jahr)
+{
+   return  (( (jahr % 4 == 0) && (jahr % 100 != 0) ) || (jahr % 400 == 0));
 }
 
-OTime now() { return OTime(); }
+Day today()
+{
+   OTime ot;
+   return ot.getDay();
+}
+
+OTime now()
+{
+   return OTime();
+}
 
 
 
@@ -41,12 +49,56 @@ Year::Year(size_t zahl) :jz(zahl) {}
 
 
 
-Year& Year::operator()(size_t j){
-	jz = j;
-	return *this;
+Year& Year::operator()(size_t j)
+{
+   jz = j;
+   return *this;
 }
 
 
+Year& Year::operator=(const Year& j)
+{
+   jz = j.jz;
+   return *this;
+}
+
+
+Year::operator bool() const
+{
+   constexpr int gregkal_start = 1582; /* Einführung gregorianischer Kalender */
+   return ( (jz > gregkal_start) && (jz < 2400) );
+}
+
+
+constexpr Year& Year::operator++()   /* Präfix-Inkrementor */
+{
+   ++jz;
+   return *this;
+}
+
+
+
+Year Year::operator++(int)   /* Postfix-Inkrementor */
+{
+   Year tmp(*this);
+   ++(*this);
+   return tmp;
+}
+
+
+constexpr Year& Year::operator--()   /* Präfix-Inkrementor */
+{
+   --jz;
+   return *this;
+}
+
+
+Year Year::operator--(int)   /* Postfix-Inkrementor */
+{
+   Year tmp(*this);
+   --(*this);
+   return tmp;
+}
 
 
 
@@ -59,49 +111,85 @@ Year& Year::operator()(size_t j){
 Month::Month(size_t zahl, const Year& j) : mz(zahl), jahr(j) {}
 
 
-
-Month::Month(const OTime* ptr) {
-	if (! ptr) {
-		OTime jetzt;
-		ptr = &jetzt;
-	}
-	mz = ptr->mm();
-	jahr(ptr->jj());
-}
+Month::Month(const OTime& t) : mz(t.mm()), jahr(t.jj()) {}
 
 
-Month& Month::operator()(size_t mm, const Year& j) {
-	mz = mm;
-	jahr = j;
-	return *this;
+Month& Month::operator()(size_t mm, const Year& j)
+{
+   mz = mm;
+   jahr = j;
+   return *this;
 }
 
 
 /* Validiere: */
-Month::operator bool() const {
-	return ((mz) && (mz < 13));
+Month::operator bool() const
+{
+   return ((mz > 0) && (mz < 13) && jahr);
+}
+
+Month& Month::operator++()
+{
+   if (++mz > 12) {
+      ++jahr;
+      mz = 1;
+   }
+   return *this;
 }
 
 
-int Month::days() const {
-	switch (mz) {
-	case 4:
-	case 6:
-	case 9:
-	case 11:
-		return 30;
-	case 2:
-		return (jahr.istSchaltjahr()) ? 29 : 28;
-	default:
-		return 31;
-	}
+Month Month::operator++(int)
+{
+   Month tmp = *this;
+   ++(*this);
+   return tmp;
+}
+
+Month& Month::operator--()
+{
+   if (--mz < 1) {
+      --jahr;
+      mz = 12;
+   }
+   return *this;
 }
 
 
-void Month::validate() const {
-	if (! *this)
-		throw OException::IllegalArgumentException("Ungültige Monatszahl" + std::to_string(mz));
+Month Month::operator--(int)
+{
+   Month tmp = *this;
+   --(*this);
+   return tmp;
 }
+
+
+
+std::string Month::name() const
+{
+   constexpr const char* namen[] {"Jänner", "Februar", "März", "April", "Mai", "Juni",
+                                  "Juli", "August", "September", "Oktober", "November", "Dezember"
+                                 };
+   if (! *this) return "Ungültig";
+   return namen[mz-1];
+}
+
+
+int Month::days() const
+{
+   switch (mz) {
+   case 4:
+   case 6:
+   case 9:
+   case 11:
+      return 30;
+   case 2:
+      return (jahr.istSchaltjahr()) ? 29 : 28;
+   default:
+      return 31;
+   }
+}
+
+
 
 
 
@@ -117,96 +205,73 @@ void Month::validate() const {
  * */
 
 
-inline void Day::init(const Zeit::OTime* ptr) {
-	if (! ptr) {
-		Zeit::OTime jetzt;
-		ptr = &jetzt;
-	}
-	tz    = ptr->tt();
-	monat = ptr->getMonth();
+inline void Day::init(const Zeit::OTime* ptr)
+{
+   if (! ptr) {
+      Zeit::OTime jetzt;
+      ptr = &jetzt;
+   }
+   tz    = ptr->tt();
+   monat = ptr->getMonth();
 }
 
 
 /* private, nicht sichtbare Funktion! *
  * size(buf) muß >= 3 !!!             */
-void myparse(int* buf, istream& parse, const Zeit::OTime* ot = nullptr) {
-	int bufpos = -1; // nichts geschrieben...
-
-	while (parse) {
-		static int i;
-		parse >> i;
-		if (parse.fail()) break;
-		buf[++bufpos] = abs(i); // allf. Vorzeichen ignorieren...
-		parse.ignore(); // Trennzeichen...
-	}
-
-	Zeit::OTime jetzt;
-	if (bufpos < 2) { // Datum noch unvollständig...
-		if (! ot) {
-			ot = &jetzt;
-		}
-		buf[2] = ot->jj();
-		if (bufpos < 1) buf[1] = ot->mm();
-		if (bufpos < 0) buf[0] = ot->tt();
-	}
-	/* Datum validieren: */
-	bool fehler = false;
-	if ((buf[1] > 12) || (buf[1] < 1) || (buf[0] < 1))
-		{ fehler = true; }
-	else {
-		switch (buf[1]) {
-		case 2:
-		{
-			int maxday = Zeit::istSchaltjahr(buf[2]) ? 29 : 28;
-			if (buf[0] > maxday) { fehler = true; }
-			break;
-		}
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			if (buf[0] > 30) { fehler = true; }
-			break;
-		default:
-			if (buf[0] > 31) { fehler = true; }
-		}
-	}
-	if (fehler) {
-		ostringstream os;
-		os << "Ungültiges Datum: " << buf[0] << "." << buf[1] << "." << buf[2];
-		throw OException::IllegalArgumentException(os.str());
-	}
+void myparse(int* buf, istream& parse, const Zeit::OTime* ot = nullptr)
+{
+   int bufpos = -1; // nichts geschrieben...
+   while (parse) {
+      static int i;
+      parse >> i;
+      if (parse.fail()) break;
+      buf[++bufpos] = abs(i); // allf. Vorzeichen ignorieren...
+      parse.ignore(); // Trennzeichen...
+   }
+   Zeit::OTime jetzt;
+   if (bufpos < 2) { // Datum noch unvollständig...
+      if (! ot) {
+         ot = &jetzt;
+      }
+      buf[2] = ot->jj();
+      if (bufpos < 1) buf[1] = ot->mm();
+      if (bufpos < 0) buf[0] = ot->tt();
+   }
+   else if (buf[2] < 100) buf[2] += 2000;   /* bei zweistelliger Jahreseingabe */
 }
 
 
-inline void Day::parse(istream& parse, const Zeit::OTime* ot = nullptr) {
-	int buf[3] {0};
-	myparse(buf, parse, ot);
-	tz   = buf[0];
-	monat(buf[1], buf[2]);
+inline void Day::parse(istream& parse, const Zeit::OTime* ot = nullptr)
+{
+   int buf[3] {0};
+   myparse(buf, parse, ot);
+   tz   = buf[0];
+   monat(buf[1], buf[2]);
 }
 
 
-Day parseDay(std::istream& parse, const Zeit::OTime* ot) {
-	int buf[3] {0};
-	myparse(buf, parse, ot);
-	Day ret(buf[0], buf[1], buf[2]);
-	return ret;
+Day parseDay(std::istream& parse, const Zeit::OTime* ot)
+{
+   int buf[3] {0};
+   myparse(buf, parse, ot);
+   Day ret(buf[0], buf[1], buf[2]);
+   return ret;
 }
 
 
 
-//Day::Day() : Day(Zeit::now()) {}
 Day::Day() { /* Alle Members sind bereits definiert */ }
 
 
-Day::Day(const std::string& param, const Zeit::OTime* ot) {
-	if (param.empty()) {
-		init(ot);
-	} else {
-		std::istringstream is(param);
-		parse(is, ot);
-	}
+Day::Day(const std::string& param, const Zeit::OTime* ot)
+{
+   if (param.empty()) {
+      init(ot);
+   }
+   else {
+      std::istringstream is(param);
+      parse(is, ot);
+   }
 }
 
 
@@ -219,60 +284,106 @@ Day::Day(size_t t, size_t m, size_t j) : monat(m, j), tz(t) {}
 Day::Day(size_t tt, const Month& m) : monat(m), tz(tt) {}
 
 
-OTime Day::getTime() const {
-	return OTime(tz, monat.mm(), monat.year().jj());
+OTime Day::getTime() const
+{
+   return OTime(tz, monat.mm(), monat.year().jj());
 }
 
 
-string Day::toString() const {
-	char buf[11];
-	sprintf(buf, "%02d.%02d.%04d", tz, monat.mm(), monat.year().jj());
-	return buf;
+Day& Day::operator++()
+{
+   if (static_cast<int>(++tz) > monat.days()) {
+      tz = 1;
+      ++monat;
+   }
+   return *this;
+}
+
+Day Day::operator++(int)
+{
+   Day tmp(*this);
+   ++(*this);
+   return tmp;
+}
+
+Day& Day::operator--()
+{
+   if (static_cast<int>(--tz) < 1) {
+      --monat;
+      tz = monat.days();
+   }
+   return *this;
+}
+
+Day Day::operator--(int)
+{
+   Day tmp(*this);
+   --(*this);
+   return tmp;
+}
+
+
+string Day::toString() const
+{
+   char buf[11];
+   sprintf(buf, "%02d.%02d.%04d", static_cast<int>(tz), monat.mm(), monat.year().jj());
+   return buf;
 }
 
 
 
-std::string Day::toSQL() const {
-	char buf[11];
-	sprintf(buf, "%4d-%02d-%02d", monat.year().jj(), monat.mm(), static_cast<int>(tz));
-	return buf;
+std::string Day::toSQL() const
+{
+   char buf[11];
+   sprintf(buf, "%4d-%02d-%02d", monat.year().jj(), monat.mm(), static_cast<int>(tz));
+   return buf;
 }
 
-Day Day::deltaDays(int i) {
-	return getTime().deltaDays(i);
-}
-
-
-
-Day::operator bool() const {
-	return monat && (tz > 0) && (tz <= monat.days());
+Day Day::deltaDays(int i)
+{
+   return getTime().deltaDays(i);
 }
 
 
 
-ostream& operator<<(ostream& os, const Day& td) {
-	return os << td.toString();
-}
-
-
-std::istream& operator>>(std::istream& is, Day& td) {
-	td.parse(is, nullptr);
-	return is;
+Day::operator bool() const
+{
+   return monat && (tt() <= monat.days());
 }
 
 
 
-
-SQLDay::SQLDay(std::string sql) {
-	setValues(sql);
+ostream& operator<<(ostream& os, const Day& td)
+{
+   const char forig = os.fill('0');
+   for (int i : {td.tt(), td.mm()}) os << setw(2) << i << '.';
+   os << setw(4) << td.jj();
+   os.fill(forig);
+   return os;
 }
 
 
-void SQLDay::setValues(const std::string& sql) {
-	int y = stoi(sql.substr(0,4));
-	int m = stoi(sql.substr(5,2));
-	int d = stoi(sql.substr(8,2));
-	setValues(d,m,y);
+std::istream& operator>>(std::istream& is, Day& td)
+{
+   td.parse(is, nullptr);
+   return is;
+}
+
+
+
+
+SQLDay::SQLDay(std::string sql)
+{
+   setValues(sql);
+}
+
+
+void SQLDay::setValues(const std::string& sql)
+{
+   int y = stoi(sql.substr(0,4));
+   int m = stoi(sql.substr(5,2));
+   int d = stoi(sql.substr(8,2));
+   setValues(d,m,y);
 }
 
 
@@ -283,58 +394,94 @@ void SQLDay::setValues(const std::string& sql) {
  *                          Klasse: OTime
  * */
 
-inline void OTime::init(long int epoch) {
-	myTime = static_cast<time_t>(epoch);
-	localtime_r(&myTime, &localTime);
+inline void OTime::init(long int epoch)
+{
+   myTime = static_cast<time_t>(epoch);
+   localtime_r(&myTime, &localTime);
 }
 
-OTime::OTime() {
-	init(time(0));
+OTime::OTime()
+{
+   init(time(0));
 
 }
 
-OTime::OTime(const char* timestr, const char* format) {
-	// Siehe dazu man strptime...
-	strptime(timestr, format, &localTime);
-	myTime = mktime(&localTime);
+OTime::OTime(const char* timestr, const char* format)
+{
+   // Siehe dazu man strptime...
+   strptime(timestr, format, &localTime);
+   myTime = mktime(&localTime);
 }
 
-OTime::OTime(int tag, int monat, int jahr) {
-	localTime.tm_sec = localTime.tm_min = localTime.tm_hour = 0;
-	localTime.tm_mday = tag;
-	localTime.tm_mon = monat - 1;
-	localTime.tm_year = jahr - 1900;
-	localTime.tm_isdst = 0; // Sommerzeit ausschalten
-	myTime = mktime(&localTime);
+OTime::OTime(int tag, int monat, int jahr)
+{
+   localTime.tm_sec = localTime.tm_min = localTime.tm_hour = 0;
+   localTime.tm_mday = tag;
+   localTime.tm_mon = monat - 1;
+   localTime.tm_year = jahr - 1900;
+   localTime.tm_isdst = 0; // Sommerzeit ausschalten
+   myTime = mktime(&localTime);
 }
 
-OTime::OTime(long int epoch) {
-	init(epoch);
+OTime::OTime(long int epoch)
+{
+   init(epoch);
 }
 
 
-string OTime::formatiere(const char* format, size_t capacity) const {
-	// Wenn capacity zu klein gewählt ist, dann ist
-	// der Output undefiniert!
-	char buf[++capacity]; // Endzeichen hinzurechnen!
-	OTime::formatiere(buf, capacity, format);
-	return buf;
+string OTime::formatiere(const char* format, size_t capacity) const
+{
+   // Wenn capacity zu klein gewählt ist, dann ist
+   // der Output undefiniert!
+   char buf[++capacity]; // Endzeichen hinzurechnen!
+   OTime::formatiere(buf, capacity, format);
+   return buf;
 }
 
-size_t OTime::formatiere(char* chbuf, size_t bufsize, const char* format) const {
-    return strftime(chbuf, bufsize, format, &localTime); }
-
-
-struct tm OTime::localtime() const { return localTime; }
-
-
-Day OTime::deltaDays(int d) const {
-	OTime t(epoch() + (d * 86400));
-	return Day(t);
+size_t OTime::formatiere(char* chbuf, size_t bufsize, const char* format) const
+{
+   return strftime(chbuf, bufsize, format, &localTime);
 }
 
-ostream& operator<<(ostream& os, OTime& t) {
-	return os << t.now19();
+
+struct tm OTime::localtime() const {
+   return localTime;
 }
+
+
+Day OTime::deltaDays(int d) const
+{
+   OTime t(epoch() + (d * 86400));
+   return Day(t);
+}
+
+ostream& operator<<(ostream& os, OTime& t)
+{
+   return os << t.now19();
+}
+
+
+
+void testAll() {
+   std::cout << "Teste otime:\n";
+   std::cout << "Heute ist der " << Zeit::today() << '\n';
+   Zeit::Day tag1;
+   std::cout << "Undefiniertes Datum: " << tag1 << '\n';
+   std::string str{"2.4.12"};
+   Zeit::Day tag2(str);
+   std::cout << "Formatiere '" << str << "' zu " << tag2 << '\n';
+   std::cout << "Monatsname ist " << tag2.getMonth().name() << ".\n";
+   std::cout << "Teste Operatoren ++ und --:\n";
+   Zeit::Day tag3("29.12.2008");
+   constexpr int maxi{5};
+   for (int i = 0; i < maxi; ++i) {
+      cout << tag3++ << '\n';
+   }
+   for (int i = 0; i < maxi; ++i) {
+      cout << tag3-- << '\n';
+   }
+   cout << "Ende Test otime.\n";
+}
+
 
 } // namespace Zeit
